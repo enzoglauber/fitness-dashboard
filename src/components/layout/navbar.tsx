ec96e4b02cfd84e198b2332f2b9cd9fe29360608
Enzo Glauber <enzojob@gmail.com>
2025-02-25 22:54:58 -0300

    fixed navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,8 +1,25 @@
-import { ToggleTheme } from '@/components/toogle-theme'
+import { ToggleTheme } from "@/components/toogle-theme";
+import { useEffect, useState } from "react";
 
 export const Navbar = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sticky = window.scrollY > 68
+      setIsSticky(sticky);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="shadow-sm bg-gray-100 dark:bg-gray-900">
+    <header
+      className={`transition-all duration-300 shadow-sm bg-gray-100 dark:bg-gray-900 ${isSticky ? "fixed top-0 left-0 w-full z-50" : ""}`}
+    >
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8"
         aria-label="Global"
@@ -19,5 +36,5 @@ export const Navbar = () => {
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
